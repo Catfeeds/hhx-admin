7b49b56c0510efe246dd0217b26cd2f846f8cf66
hhx03 <1007502956@qq.com>
2019-10-12 15:30:28 +0800

    名字更改

--- app/Admin/Controllers/YongLeController.php
+++ app/Admin/Controllers/YongLeController.php
@@ -3,6 +3,7 @@
 namespace App\Admin\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\YongLe;
 use Encore\Admin\Controllers\HasResourceActions;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
